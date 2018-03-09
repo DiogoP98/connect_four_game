ee2fb7b79fa01e1c319222d639fa2ae2868a380d
afonsonf <afonsofernandes@netcabo.pt>
2018-03-09 22:48:02 +0000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,5 +10,13 @@ int main(){
 	a->play(0,2);
 	a->play(0,2);
 	a->print_board();
+	printf("------------------\n\n");
+	vector<Node> l = a->child_list(1);
+	for(int i=0;i<(int)l.size();i++)
+		l[i].print_board();
+	l = l[0].child_list(2);
+	printf("------------------\n\n");
+	for(int i=0;i<(int)l.size();i++)
+		l[i].print_board();
 	return 0;
 }
