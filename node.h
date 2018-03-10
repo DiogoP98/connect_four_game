8966498c0e848827ec6f18436891f1e36c445895
afonsonf <afonsofernandes@netcabo.pt>
2018-03-10 15:56:28 +0000

    Update node.h

--- node.h
+++ node.h
@@ -10,7 +10,7 @@
 class Node{
 public:
 	std::pair<int,int> conf[7];
-	int last_play; //column
+	int last_play; 
 	int val;
 	
 	Node();
@@ -19,7 +19,7 @@ public:
 	int play(int column,int player);
 	int pos(int i,int j);
 	std::vector<Node> child_list(int player);
-	int final_board();//1-player 1 wins; 2-player 2 wins; 0 draw, -1 no final
+	int final_board();
 	int eval(int i, int j);
 	int eval_board();
 
