891bd89fbdee8466da9dfb4fbd545429dbeaa2ff
afonsonf <afonsofernandes@netcabo.pt>
2018-03-10 14:52:30 +0000

    Update node.h

--- node.h
+++ node.h
@@ -20,6 +20,8 @@ public:
 	int pos(int i,int j);
 	std::vector<Node> child_list(int player);
 	int final_board();//1-player 1 wins; 2-player 2 wins; 0 draw, -1 no final
+	int eval(int i, int j);
+	int eval_board();
 
 	void print_board();
 };
