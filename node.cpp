0937528c5dbad71a7bb3692176bfd3c793ccc5c0
afonsonf <afonsofernandes@netcabo.pt>
2018-03-10 14:52:02 +0000

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,12 +1,12 @@
 #include "node.h"
 
-std::pair<int,int> unit_vector[] = {std::make_pair(-1,-1),
+std::pair<int,int> unit_vector[] = {std::make_pair( 1,-1),
+									std::make_pair( 0,-1),
+									std::make_pair(-1,-1),
 									std::make_pair(-1, 0),
 									std::make_pair(-1, 1),
 									std::make_pair( 0, 1),
-									std::make_pair( 1, 1),
-									std::make_pair( 1,-1),
-									std::make_pair( 0,-1)};
+									std::make_pair( 1, 1)};
 
 Node::Node(){
 	for(int i=0;i<7;i++){
@@ -75,7 +75,6 @@ int Node::final_board(){
 		ix+=unit_vector[k].first;
 		jx+=unit_vector[k].second;
 		while(ix>=0 && ix<6 && jx>=0 && jx<7){
-			//printf("")
 			if(pos(ix,jx)!=x)break;
 			s++;
 			ix+=unit_vector[k].first;
@@ -86,6 +85,71 @@ int Node::final_board(){
 	
 	if(!flag) return 0;
 	return -1;
+}
+
+//<flag,val>
+int Node::eval(int i, int j){
+	int val=0;
+	int x;
+	int ix,jx,l,s,xx;
+		
+	int sign;
+	//printf("%d %d\n",i,j);
+	for(int k=0;k<4;k++){
+		l=0;
+		
+		x = pos(i,j);
+		if(x!=0)s=1;
+		else s=0;
+		
+		ix=i+unit_vector[k].first;
+		jx=j+unit_vector[k].second;
+		while(ix>=0 && ix<6 && jx>=0 && jx<7 && l<3){
+			xx = pos(ix,jx);
+			if(x==0 && xx!=0)x=xx;
+			
+			if(x!=0 && xx==x)s++;
+			else if(xx!=0) break;
+			l++;
+			ix+=unit_vector[k].first;
+			jx+=unit_vector[k].second;
+		}
+		if(l==3 && x!=0){
+			if(x==1)sign=-1;
+			else sign=1;
+			
+			switch(s){
+				case 1:
+					val+=sign*1;
+					break;
+				case 2:
+					val+=sign*10;
+					break;
+				case 3:
+					val+=sign*50;
+					break;
+				case 4:
+					val+=sign*512;
+					break;
+			}
+		}
+		//printf("\t%2d | %d %d",val,unit_vector[k].first,unit_vector[k].second);
+	}
+	//printf("\n---------------------------------\n");
+	return val;
+}
+
+int Node::eval_board(){
+	int val=0;
+	for(int i=5;i>=0;i--){
+		for(int j=0;j<7;j++){
+			if(!(i<3 && j<3)){
+				val+=eval(i,j);
+			}
+		}
+		//printf("---------------------------------\n");
+	}
+	return val;
 }
 
 void Node::print_board(){
