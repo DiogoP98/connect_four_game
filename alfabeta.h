776774133b7bdeb39be6be34c007a3367f096313
DiogoP98 <diogo99p@gmail.com>
2018-03-11 14:12:46 +0000

    Add files via upload

--- alfabeta.h
+++ alfabeta.h
@@ -6,5 +6,8 @@
 #include "node.h"
 
 int AlfaBeta(Node *a);
+int min_value(Node  *a, int alfa, int beta, int depth_in, int depth_max);
+int max_value(Node *a, int alfa, int beta, int depth_in, int depth_max)
+
 
 #endif // MINIMAX_H
