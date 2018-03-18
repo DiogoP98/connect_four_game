4bcfc2b0781c2117264be8e1fb846ee8f3d9e2e3
afonsonf <afonsofernandes@netcabo.pt>
2018-03-18 14:26:24 +0000

    Update minimax.h

--- minimax.h
+++ minimax.h
@@ -2,12 +2,13 @@
 #define MINIMAX_H
 
 #include <stdio.h>
+#include <limits.h>
 
 #include "node.h"
-
 class Minimax{
 public:
- static int minimax(Node *a);
+	static int minimax(Node *a,int k,int d);
+	static int max_value(Node *a,int depth_max);
+	static int min_value(Node *a,int depth_max); 
 };
-
 #endif // MINIMAX_H
