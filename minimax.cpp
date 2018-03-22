bbb85e2bd8b09795bd33fbef17f6773303afe9a7
afonsonf <afonsofernandes@netcabo.pt>
2018-03-22 21:16:32 +0000

    Update minimax.cpp

--- minimax.cpp
+++ minimax.cpp
@@ -1,6 +1,6 @@
 #include "minimax.h"
 
-long long expandidos2;
+long long expandidos2;//number of nodes expanded
 
 int Minimax::minimax(Node *a,int k, int depthx){
 	expandidos2 = 0;
@@ -24,37 +24,28 @@ int Minimax::max_value(Node *a,int depth_max){
 			case 0:
 				return    0;
 			default:
-				//printf("%d\n",a->depth);
-				//a->print_board();
 				return a->eval_board();
 		}
 	}
 	int val = INT_MIN,x,lp,kk;
 	lp=a->last_play;
-	//std::vector<Node*> l = a->child_list(2);
 	for(int i=0;i<7;i++){
 		
-		//a->print_board();
 		kk=a->play(i,2);
-		//printf("a %d\n",kk);
 		
 		if(!kk){
 			expandidos2++;
 			a->depth++;
 			x = min_value(a,depth_max);
-			//printf("%d\n",x);
-			//if(x>600)a->print_board();
-			//exit(1);
+
+			//for printing value of childs of first 2 levels
 			if(opt==1){
 				printf("\t%d = %d (0)\n",a->last_play,x);
-				//s[i]->print_board();
 			}
 			if(opt==2){
 				printf("\t\t%d = %d (1)\n",a->last_play,x);
-				//s[i]->print_board();
 			}
 			if(val<x){
-				//printf("%d\n",a->depth-depth_in);
 				if(a->depth==1)
 					a->best_play=a->last_play;
 				val = x; 
@@ -64,8 +55,6 @@ int Minimax::max_value(Node *a,int depth_max){
 			//delete l[i];
 		}
 	}
-	//printf("%d\n",val);
-	//a->print_board();
 	return val;
 }
 
@@ -83,47 +72,36 @@ int Minimax::min_value(Node *a,int depth_max){
 			case 0:
 				return    0;
 			default:
-				//printf("%d\n",a->depth);
-				//a->print_board();
 				return a->eval_board();
 		}
 	}
 	int val = INT_MAX,x,lp,kk;
 	lp=a->last_play;
-	//std::vector<Node*> l = a->child_list(1);
 	for(int i=0;i<7;i++){
 		
-		//a->print_board();
 		kk=a->play(i,1);
-		//printf("b %d\n",kk);
 		
 		if(!kk){
 			expandidos2++;
 			a->depth++;
 			x = max_value(a,depth_max);
-			//printf("%d\n",x);
-			//if(x>600)a->print_board();
-			//exit(1);
+			
+			//for printing value of childs of first 2 levels
 			if(opt==1){
 				printf("\t%d = %d (0)\n",a->last_play,x);
-				//s[i]->print_board();
 			}
 			if(opt==2){
 				printf("\t\t%d = %d (1)\n",a->last_play,x);
-				//s[i]->print_board();
 			}
+			
 			if(val>x){
-				//printf("%d\n",a->depth-depth_in);
 				if(a->depth==1)
 					a->best_play=a->last_play;
 				val = x; 
 			}
 			a->depth--;
 			a->rmplay(lp);
-			//delete l[i];
 		}
 	}
-	//printf("%d\n",val);
-	//a->print_board();
 	return val;
 }
